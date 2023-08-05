d52895c0195aafba4c167379da7ad95acd505894
Carla Valdivia <valdivia.carla@gmail.com>
2023-08-05 01:01:06 -0300

    tests and md

---
+++ array/change_the_array/README.md
@@ -0,0 +1,7 @@
+## Change An Array
+
+### TO DO: use this method to change the array.
+
+1. If the action is "add" your method should push an element inside the array
+
+2. If the action is "delete", your method should delete an element of the array
--- array/change_the_array/change_array.rb
+++ array/change_the_array/change_array.rb
@@ -1,10 +1,3 @@
-# TO DO: use this method change the array.
-
-# 1. If the action is "add" your method should push an element inside the array
-
-# 2. If the action is "delete", your method should delete an element of the array
-
-
 def adding_something_new(array, element, action)
   # TO DO
 end
